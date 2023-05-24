0e1be8f5aefe15d1291077b22fb79aa5b837e1f8
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-05-24 16:15:31 +0800

    adding a text-editor

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,12 +1,26 @@
 <template lang="">
     <div>
-        Hello World
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
     </div>
 </template>
 <script>
-import 
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
 export default {
     name: 'CreatePost',
+    components: {
+        ckeditor: CKEditor.component
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            editorData: '<p>Content of the editor.</p>',
+            editorConfig: {
+                // The configuration of the editor.
+            }
+        }
+    }
 }
 </script>
 <style lang="">
